8a64c7ef7c04d56b2d4330e8a0731d2a5f1d8bac
Shade-v2006 <Shade-v2006@yandex.ru>
2024-02-09 00:24:13 +0700

    Решена задача1

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -4,37 +4,64 @@
 */
 // Условие: Взять число проверить его на четность
 
-int GetArrayFrCons() // Функция чтения цифр из консоли
+string GetArrayFrCons() // Функция чтения цифр из консоли
 {
     Console.Write("Enter number: ");
-    int size = int.Parse(Console.ReadLine()!);
+    string size = (Console.ReadLine()!);
     return size;
 }
-void PrintArray(int digit) //  Функция для вывода текста на экран
+
+int ParityCheck(string size) // Функция проверки нажатия "q" изменения массива в цифру и сложения цифр с проверкой на четность
 {
-    Console.Write("[ ");
-    Console.Write(digit + " ");
-    Console.WriteLine("]");
-}
-int ParityCheck(int size) // Функция изменения массива в цифру
-{
+    int num = 0;
+    int sumNumber = 0;
+    int sumNumber2 = 0;
     while (true)
     {
-        
-        //System.Console.WriteLine(size);
-        if (size % 2 == 0)
+
+
+        if (size == "q")
         {
-            System.Console.WriteLine("Число чётно!");
+            System.Console.WriteLine("END!");
             break;
         }
-        size = GetArrayFrCons();        
+        else
+        {
+            int digit = int.Parse(size);
+            num = digit;
+            int y = (int)Math.Log10(num) + 1; // Например, число 12345. Десятичный логарифм его равен примерно 4,091. Приводим к целому → 4. Плюс 1= 5. В числе 5 цифр.
+            //System.Console.WriteLine(y);
+
+            if (y == 1 && num % 2 == 0)
+            {
+                System.Console.WriteLine("Число четное!");
+                break;
+            }
+            else
+            {
+                for (int i = 0; i < y; i++)
+                {
+                    sumNumber = num % 10;
+                    num /= 10;
+                    sumNumber2 += sumNumber;
+                }
+                if (sumNumber2 % 2 == 0)
+                {
+                    System.Console.WriteLine("Число четное! "+ sumNumber2);
+                    break;
+                }
+                System.Console.WriteLine("Сумма цифр числа нечетная: " + sumNumber2);
+                sumNumber = 0;
+                sumNumber2 = 0;
+                size = GetArrayFrCons();
+            }
+        }
     }
-    int num = 1;
     return num;
 }
 
+string size = GetArrayFrCons();
+int num = ParityCheck(size);
 
 
 
-
-//PrintArray(digit); // Передача в функцию аргументов для печати
